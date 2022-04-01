47dd74359dbc3209982e87ddd8b0f3c8d9059d0a
amimenezes <mi.menezes.luiz@gmail.com>
2022-03-31 23:42:37 -0300

    Realizando Testes em Action-v2

--- service/src/test/java/dev/rpmhub/NativeClientIT.java
+++
@@ -1,9 +0,0 @@
-package dev.rpmhub;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeClientIT extends ClientTest {
-
-    // Execute the same tests but in native mode.
-}
