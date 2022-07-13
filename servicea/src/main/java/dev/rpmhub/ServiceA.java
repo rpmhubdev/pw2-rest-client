663e8576f4177ef45ae82e362f568521e727b926
Caroline Scherer <scherer.caroline@gmail.com>
2022-07-13 20:42:44 -0300

    Exercício rest-client

--- servicea/src/main/java/dev/rpmhub/ServiceA.java
+++ servicea/src/main/java/dev/rpmhub/ServiceA.java
@@ -15,11 +15,15 @@
  */
 package dev.rpmhub;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/servicea")
 public class ServiceA {
@@ -28,13 +32,16 @@ public class ServiceA {
     // Por meio de injeção de dependência,
     // instancie o Rest Cliente para o serviço B
 
+    @Inject
+    @RestClient
+
+    ServiceB service;
+
     @GET
     @Path("/person/{name}")
     @Produces(MediaType.APPLICATION_JSON)
     public Person getPerson(@PathParam("name") String name){
-        // TODO
-        // Complete o método
-        return null;
+        return service.getPerson(name);
     }
 
 }
